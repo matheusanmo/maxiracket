bdb18bc43ebc5b104df65f2d832a47fa82bfd932
matheus anmo <matheusanmo@gmail.com>
2022-03-27 19:26:00 -0300

    commit inicial

---
+++ main.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(require racket/system) ; system
+
+; (system "ls" #:set-pwd? #t)
+
+; (read-char (current-input-port))
+
+; (write-string "bom dia flor do dia" (current-output-port))
+
+
+
